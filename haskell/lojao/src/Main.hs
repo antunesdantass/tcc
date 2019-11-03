51d251b69107bc21a80d3f0fe1a3346907ab84b3
Antunes Dantas <antunesdantas@outlook.com>
2019-11-03 15:36:33 -0300

    finished second use case in haskell

--- haskell/lojao/src/Main.hs
+++ haskell/lojao/src/Main.hs
@@ -1,8 +1,8 @@
 module Main where
-  
-  import Product
   import Sell
   import Checkout
+
+  data Product = Product { name :: String, price :: Float, category :: String }
 
   initial_menu = "= = = = Bem-vindo(a) ao EconomizaP2 = = = = \n" ++
     "Digite a opção desejada: \n" ++
@@ -23,8 +23,23 @@ module Main where
     option <- getLine
     runTask products (read option :: Integer)
 
+  createProduct :: String -> String -> String -> Product
+  createProduct name price category = Product { name = name, price = (read price :: Float), category = category }
+
   runTask :: [Product] -> Integer -> IO ()
-  runTask products 1 = executionLoop (createProduct products)
+  runTask products 1 = do
+    putStrLn "Qual o nome do produto?"
+    name <- getLine
+    putStrLn "Digite o preço unitário do produto"
+    price <- getLine
+    putStrLn "Digite o tipo do produto"
+    category <- getLine
+    let product = createProduct name price category
+    putStrLn (name ++ " cadastrado com sucesso!")
+    putStrLn "Deseja cadastrar outro produto?"
+    choice <- getLine
+    if (choice == "Sim") then runTask ([product] ++ products) 1 else executionLoop ([product] ++ products)
+
   runTask products 2 = sell
   runTask products 3 = checkout
   runTask products 4 = putStrLn ""
--- haskell/lojao/src/Product.hs
+++ haskell/lojao/src/Product.hs
@@ -1,44 +1,51 @@
-module Product (
-  createProduct,
-  Product
-) where
+-- module Product (
+--   createProduct,
+--   Product
+-- ) where
 
-  data Product = Product { name :: String,
-                            price :: Float,
-                            category :: String }
+--   data Product = Product { name :: String,
+--                             price :: Float,
+--                             category :: String }
 
-  createProduct :: [Product] -> [IO Product]
-  createProduct products = do
-    let product = ioCreation
-    return ([product] ++ products)
-    -- putStrLn "Deseja cadastrar outro produto?"
-    -- choice <- getLine
-    -- productsNew <- ([product] ++ products)
-    -- productsNew
-    -- if (createAnother choice) then createProduct newList
-    -- else newList
-    -- newList
+--   createProduct :: [IO Product] -> [IO Product]
+--   createProduct products = do
+--     product <- ioCreation
+--     return (addProduct product products)
+--     -- putStrLn "Deseja cadastrar outro produto?"
+--     -- choice <- getLine
+--     -- productsNew <- ([product] ++ products)
+--     -- productsNew
+--     -- if (createAnother choice) then createProduct newList
+--     -- else newList
+--     -- newList
 
-  addProduct :: Product -> [Product] -> [Product]
-  addProduct product products = [product] ++ products
+--   addProduct :: Product -> [IO Product] -> [Product]
+--   addProduct product [] = [product]
+--   addProduct product products = do
+--     cabeca <- head products
+--     return ([product] ++ (addProduct cabeca (tail products)))
 
-  ioCreation :: IO Product
-  ioCreation = do
-    putStrLn "Qual o nome do produto?"
-    name <- getLine
-    putStrLn "Digite o preço unitário do produto"
-    price <- getLine
-    putStrLn "Digite o tipo do produto"
-    category <- getLine
-    printSuccess name
-    return (createNewProduct name (read price :: Float) category)
+--   add :: Product -> [Product] -> [Product]
+--   add product [] = [product]
+--   add product products = [product] ++ (add (head products) (tail products)) 
+
+--   ioCreation :: IO Product
+--   ioCreation = do
+--     putStrLn "Qual o nome do produto?"
+--     name <- getLine
+--     putStrLn "Digite o preço unitário do produto"
+--     price <- getLine
+--     putStrLn "Digite o tipo do produto"
+--     category <- getLine
+--     printSuccess name
+--     return (createNewProduct name (read price :: Float) category)
   
-  printSuccess :: String -> IO ()
-  printSuccess productName = putStrLn (productName ++ " cadastrado com sucesso!")
+--   printSuccess :: String -> IO ()
+--   printSuccess productName = putStrLn (productName ++ " cadastrado com sucesso!")
 
-  createAnother :: String -> Bool
-  createAnother "Sim" = True
-  createAnother _ = False
+--   createAnother :: String -> Bool
+--   createAnother "Sim" = True
+--   createAnother _ = False
 
-  createNewProduct :: String -> Float -> String -> Product
-  createNewProduct name price category = Product { name = name, price = price, category = category }
+--   createNewProduct :: String -> Float -> String -> Product
+--   createNewProduct name price category = Product { name = name, price = price, category = category }
